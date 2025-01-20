6f59b5e479f4fac283460e46c52cc6a4dbade881
unknown <jbeltrano@unal.edu.co>
2025-01-19 20:38:22 -0500

    Update 2

--- src/Ejecucion/Main.java
+++ src/Ejecucion/Main.java
@@ -1,19 +1,19 @@
 package Ejecucion;
 
+import java.io.IOException;
+
 import javax.swing.SwingUtilities;
 import Front.Principal;
 
 public class Main {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new Principal("src\\DB\\base.db");
             }
         });
-        
-
         
     }
 }
