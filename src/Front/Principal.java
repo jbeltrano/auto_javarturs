9414938ce0561bcd627b73ceeed359c859f4f2aa
Juan Beltran <jbeltrano@unal.edu.co>
2025-07-23 19:58:07 -0500

    Modificacion para que los botones, del panel principal, tengan un mejor estilo

--- src/Front/Principal.java
+++ src/Front/Principal.java
@@ -2,7 +2,9 @@ package Front;
 
 import java.awt.Desktop;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.Dimension;
 import java.awt.BorderLayout;
 import Front.Panel.Ciudades.Panel_ciudad;
@@ -33,6 +35,8 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.formdev.flatlaf.FlatLightLaf;
+
+import java.awt.event.MouseAdapter;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
@@ -44,13 +48,27 @@ import Estructuras_datos.Queue;
 
 public class Principal extends JFrame{
     
+    private static final int TAMAÑO_PANEL_SECUNDARIO = 70;
+    private static final int TAMAÑO_PANEL_SECUNDARIO_ANCHO = 170;
+    private static final int TAMAÑO_BOTON = 50;
+
     private JPanel panel_secundario;
     private JPanel panel_principal2;
     private JPanel panel_informacion;
     private JPanel pan;
     private JMenuBar barra_menu;
     private JLabel label_imagen;
-    private JButton boton_extractos_mensuales;
+    private JButton boton_boton_extractos_mensuales;
+    private JButton boton_vehiculos;
+    private JButton boton_extractos;
+    private JButton boton_personas;
+    private JButton boton_cuidad;
+    private JButton boton_empleados;
+    private ImageIcon imagen_ciudades;
+    private ImageIcon imagen_vehiculos;
+    private ImageIcon imagen_extractos;
+    private ImageIcon imagen_personas;
+    private ImageIcon imagen_empleados;
     private HashMap<String, Queue<String[]>> read_links;
     private HashSet<String> id_links;
     private int color_principal;
@@ -65,6 +83,7 @@ public class Principal extends JFrame{
         super("Javarturs");
         Leer_config config = new Leer_config();
         
+        
         if(config.get_tema() == 0){
             FlatLightLaf.setup();
             color_principal = config.get_color_principal();
@@ -77,14 +96,6 @@ public class Principal extends JFrame{
         }
 
         config = null;
-        // try{
-        //     UIManager.setLookAndFeel(new FlatLightLaf());
-        //     //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-
-
-        //     }catch(Exception ex){
-        //       System.out.println(ex);
-        // }
 
         setPreferredSize(new Dimension(1200,700));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -116,6 +127,23 @@ public class Principal extends JFrame{
         // Carga de imagen principal
         ImageIcon imagen1 = new ImageIcon(getClass().getResource("/Front/Recursos/imagen_principal.png"));
         label_imagen = new JLabel(imagen1);
+
+        // Carga de imagenes para los botones
+        imagen_ciudades = new ImageIcon(getClass().getResource("/Front/Recursos/Imagen_ciudades.png"));
+        imagen_ciudades = new ImageIcon(imagen_ciudades.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH));
+
+        imagen_vehiculos = new ImageIcon(getClass().getResource("/Front/Recursos/Imagen_vehiculos.png"));
+        imagen_vehiculos = new ImageIcon(imagen_vehiculos.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH));
+
+        imagen_extractos = new ImageIcon(getClass().getResource("/Front/Recursos/Imagen_extractos.png"));
+        imagen_extractos = new ImageIcon(imagen_extractos.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH));
+
+        imagen_personas = new ImageIcon(getClass().getResource("/Front/Recursos/Imagen_personas.png"));
+        imagen_personas = new ImageIcon(imagen_personas.getImage().getScaledInstance(70, 70, Image.SCALE_SMOOTH));
+
+        imagen_empleados = new ImageIcon(getClass().getResource("/Front/Recursos/Imagen_empleados.png"));
+        imagen_empleados = new ImageIcon(imagen_empleados.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH));
+
 
         // Inicializacion de los componentes a utilizar
         JPanel panel_principal = new JPanel(new BorderLayout());
@@ -226,6 +254,61 @@ public class Principal extends JFrame{
     }
 
     /**
+     * Este metodo se encarga de establecer el ancho del panel secundario
+     * cuando el mouse entra o sale de este, para que se vea mas amplio
+     * @return
+     */
+    private MouseAdapter set_ancho_secundario(){
+        return new MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                panel_secundario.setPreferredSize(new Dimension(TAMAÑO_PANEL_SECUNDARIO_ANCHO,panel_secundario.getHeight()));
+                panel_secundario.revalidate();
+                panel_secundario.repaint();
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                panel_secundario.setPreferredSize(new Dimension(TAMAÑO_PANEL_SECUNDARIO,panel_secundario.getHeight()));
+                panel_secundario.revalidate();
+                panel_secundario.repaint();
+            }
+        };
+    }
+
+    /**
+     * Este metodo se encarga de cambiar el color de fondo
+     * de los botones cuando el mouse entra o sale de este
+     * @param boton El boton al que se le cambiara el color de fondo
+     */
+    public static void back_ground_color(JButton boton){
+        boton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e) {
+                boton.setBackground(new Color(0x000000));
+            }
+
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e) {
+                boton.setBackground(null);
+
+            }
+        });
+    }
+
+    public static void config_label(JLabel label, JButton boton){
+
+        label.setBounds(
+            boton.getX() + boton.getWidth() + 10,
+            boton.getY() + (boton.getHeight() - 20)/2,
+            100,
+            20);
+
+        label.setForeground(Color.black);
+
+    }
+
+    /**
      * Se encarga de configurar el panel secundario
      * de la interfaz grafica, en este caso, la parte que
      * lleva los botones en la parte lateral izquierda de
@@ -234,58 +317,99 @@ public class Principal extends JFrame{
     private void configuracion_panel_secundario(){
 
         // Creacio componentes auxiliares
-        JButton extractos = new JButton();
-        JButton base_personas = new JButton();
-        JButton base_ciudad = new JButton();
+        boton_extractos = new JButton();
+        boton_personas = new JButton();
+        boton_cuidad = new JButton();
+        boton_vehiculos = new JButton();
+        boton_empleados = new JButton();
+
+        // Labels para los botones
+        JLabel label_boton_extractos = new JLabel("Extractos");
+        JLabel label_boton_personas = new JLabel("Personas");
+        JLabel label_boton_empleados = new JLabel("Empleados");
+        JLabel label_boton_ciudad = new JLabel("Ciudades");
+        JLabel label_boton_vehiculos = new JLabel("Vehiculos");
 
         // Configuraciones del panel
         panel_secundario.setBackground(new Color(color_principal));
-        panel_secundario.setPreferredSize(new Dimension(120,this.getHeight()));
+        panel_secundario.setPreferredSize(new Dimension(TAMAÑO_PANEL_SECUNDARIO,this.getHeight()));
+        
 
         // Cre_ de componentes y configuracion de los mismos
-        base_ciudad.setText("Ciudades");
-        base_ciudad.setBounds(10,10,100,20);
-        base_ciudad.addActionListener(_ ->{
+        boton_cuidad.setIcon(imagen_ciudades);
+        boton_cuidad.setBounds(10,10,TAMAÑO_BOTON,TAMAÑO_BOTON);
+        boton_cuidad.addActionListener(_ ->{
             configuracion_ciudad();
         });
 
-        JButton base_vehiculos = new JButton("Vehiculos");
-        base_vehiculos.setBounds(10,40,100,20);
+        boton_vehiculos.setIcon(imagen_vehiculos);
+        boton_vehiculos.setBounds(10,boton_cuidad.getY() + TAMAÑO_BOTON + 10,TAMAÑO_BOTON,TAMAÑO_BOTON);
         
-        base_vehiculos.addActionListener(_ ->{
+        boton_vehiculos.addActionListener(_ ->{
             configuracion_vehiculos();
         });
 
-        JButton base_empleados = new JButton("Empleados");
-        base_empleados.setBounds(10,70,100,20);
+        boton_empleados.setIcon(imagen_empleados);
+        boton_empleados.setBounds(10,boton_vehiculos.getY() + TAMAÑO_BOTON + 10,TAMAÑO_BOTON,TAMAÑO_BOTON);
 
-        base_empleados.addActionListener(_ ->{
+        boton_empleados.addActionListener(_ ->{
             configuracion_empleados();
         });
 
-        base_personas.setText("Personas");
-        base_personas.setBounds(10,100,100,20);
+        boton_personas.setIcon(imagen_personas);
+        boton_personas.setBounds(10,boton_empleados.getY() + TAMAÑO_BOTON + 10,TAMAÑO_BOTON,TAMAÑO_BOTON);
 
-        base_personas.addActionListener(_ ->{
+        boton_personas.addActionListener(_ ->{
             configuracion_personas();
         });
 
-        extractos.setText("Extractos");
-        extractos.setBounds(10,130,100,20);
+        boton_extractos.setIcon(imagen_extractos);
+        boton_extractos.setBounds(10,boton_personas.getY() + TAMAÑO_BOTON + 10,TAMAÑO_BOTON,TAMAÑO_BOTON);
 
-        extractos.addActionListener(_ ->{
-            configuracion_extractos();
-            boton_extractos_mensuales.doClick();
+        boton_extractos.addActionListener(_ ->{
+            configuracion_boton_extractos();
+            boton_boton_extractos_mensuales.doClick();
         });
 
+        // Mouse listener para el ancho del panel secundario
+        panel_secundario.addMouseListener(set_ancho_secundario());
+        boton_cuidad.addMouseListener(set_ancho_secundario());
+        boton_vehiculos.addMouseListener(set_ancho_secundario());
+        boton_empleados.addMouseListener(set_ancho_secundario());
+        boton_personas.addMouseListener(set_ancho_secundario());
+        boton_extractos.addMouseListener(set_ancho_secundario());
 
+
+        // Mouse listener para el cambio de color de fondo de botones
+        back_ground_color(boton_cuidad);
+        back_ground_color(boton_vehiculos);
+        back_ground_color(boton_empleados);
+        back_ground_color(boton_personas);
+        back_ground_color(boton_extractos);
+
+        // Configuracion de los labels
+        config_label(label_boton_ciudad, boton_cuidad);
+        config_label(label_boton_vehiculos, boton_vehiculos);
+        config_label(label_boton_extractos, boton_extractos);
+        config_label(label_boton_personas, boton_personas);
+        config_label(label_boton_empleados, boton_empleados);
+
+        
         // Adicionamiento componentes
-        panel_secundario.add(base_ciudad);
-        panel_secundario.add(base_vehiculos);
-        panel_secundario.add(base_empleados);
-        panel_secundario.add(base_personas);
-        panel_secundario.add(extractos);
+        panel_secundario.add(boton_cuidad);
+        panel_secundario.add(boton_vehiculos);
+        panel_secundario.add(boton_empleados);
+        panel_secundario.add(boton_personas);
+        panel_secundario.add(boton_extractos);
 
+        // Adicionamiento de labels
+        panel_secundario.add(label_boton_ciudad);
+        panel_secundario.add(label_boton_vehiculos);
+        panel_secundario.add(label_boton_extractos);
+        panel_secundario.add(label_boton_personas);
+        panel_secundario.add(label_boton_empleados);
+
+        
         panel_secundario.revalidate();
         panel_secundario.repaint();
         
@@ -563,7 +687,7 @@ public class Principal extends JFrame{
 
     }
 
-    private void configuracion_extractos(){
+    private void configuracion_boton_extractos(){
 
         panel_informacion = new JPanel();
         panel_principal2.removeAll();
@@ -571,16 +695,16 @@ public class Principal extends JFrame{
 
         // Creacion de componentes auxiliares
         JPanel panel_izq = new JPanel(null); 
-        JLabel label_principal = new JLabel("Configuración Extractos");
-        boton_extractos_mensuales = new JButton("Mensuales");
-        JButton boton_extractos_ocasionales = new JButton("Ocasionales");
+        JLabel label_principal = new JLabel("Configuración boton_extractos");
+        boton_boton_extractos_mensuales = new JButton("Mensuales");
+        JButton boton_boton_extractos_ocasionales = new JButton("Ocasionales");
         JButton boton_contratos_mensuales = new JButton("C. Mensuales");
         JButton boton_contratos_ocasionales = new JButton("C. Ocasionales");
         JButton boton_contratante = new JButton("Contratante");
 
         // Configuracion componentes
-        boton_extractos_mensuales.setBounds(10,10,120,20);
-        boton_extractos_mensuales.addActionListener(_ ->{
+        boton_boton_extractos_mensuales.setBounds(10,10,120,20);
+        boton_boton_extractos_mensuales.addActionListener(_ ->{
 
             panel_principal2.remove(panel_informacion);
             if(panel_principal2.getComponentCount() > 2){
@@ -596,8 +720,8 @@ public class Principal extends JFrame{
         });
         
 
-        boton_extractos_ocasionales.setBounds(10,40,120,20);
-        boton_extractos_ocasionales.addActionListener(_ ->{
+        boton_boton_extractos_ocasionales.setBounds(10,40,120,20);
+        boton_boton_extractos_ocasionales.addActionListener(_ ->{
 
             panel_principal2.remove(panel_informacion);
             if(panel_principal2.getComponentCount() > 2){
@@ -611,14 +735,14 @@ public class Principal extends JFrame{
             panel_principal2.repaint();
         });
 
-        boton_contratos_mensuales.setBounds(10, boton_extractos_ocasionales.getY() + boton_extractos_ocasionales.getHeight() + 10, 120, 20);
+        boton_contratos_mensuales.setBounds(10, boton_boton_extractos_ocasionales.getY() + boton_boton_extractos_ocasionales.getHeight() + 10, 120, 20);
         boton_contratos_mensuales.addActionListener(_ ->{
             panel_principal2.remove(panel_informacion);
             if(panel_principal2.getComponentCount() > 2){
                 panel_principal2.remove(pan);
             }
             
-            // cambiar para ver extractos mensuales
+            // cambiar para ver boton_extractos mensuales
             panel_informacion = new Panel_contratos_mensuales();
 
             panel_principal2.add(panel_informacion, BorderLayout.CENTER);
@@ -632,7 +756,7 @@ public class Principal extends JFrame{
             if(panel_principal2.getComponentCount() > 2){
                 panel_principal2.remove(pan);
             }
-            // cambiar para ver extractos ocasionales
+            // cambiar para ver boton_extractos ocasionales
             panel_informacion = new Panel_contratos_ocasionales();
 
             panel_principal2.add(panel_informacion, BorderLayout.CENTER);
@@ -663,8 +787,8 @@ public class Principal extends JFrame{
         // configuracion panel izq
         panel_izq.setPreferredSize(new Dimension(140,panel_principal2.getHeight()));
         panel_izq.setBackground(new Color(color_secundario));
-        panel_izq.add(boton_extractos_mensuales);
-        panel_izq.add(boton_extractos_ocasionales);
+        panel_izq.add(boton_boton_extractos_mensuales);
+        panel_izq.add(boton_boton_extractos_ocasionales);
         panel_izq.add(boton_contratos_mensuales);
         panel_izq.add(boton_contratos_ocasionales);
         panel_izq.add(boton_contratante);
